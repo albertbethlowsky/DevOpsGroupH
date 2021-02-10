57f262ab3c264de0cf769c3e5af0040e137d1cf1
Frederik Volkers <frederikvolkers@hotmail.com>
2021-02-10 18:47:31 +0100

    Db connection secures w seed

---
+++ mvc-minitwit/Data/MvcDbContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using mvc_minitwit.Models;
+
+namespace mvc_minitwit.Data
+{
+    public class MvcDbContext : DbContext
+    {
+        public MvcDbContext (DbContextOptions<MvcDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Message> Message { get; set; }
+        //public DbSet<Follower> Follower { get; set; }
+        //public DbSet<User> User { get; set; }
+    }
+}
---
+++ mvc-minitwit/Models/SeedData.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using mvc_minitwit.Data;
+using System;
+using System.Linq;
+
+namespace mvc_minitwit.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcDbContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcDbContext>>()))
+            {
+                // Look for any movies.
+                if (context.Message.Any())
+                {
+                    Console.WriteLine("the db is not empty!") ;   // DB has been seeded
+                    foreach(var m in context.Message){
+                        Console.WriteLine(m.text);
+                    }
+                    
+                    return;
+                } else {
+                    Console.WriteLine("db it not found/or empty");
+                    return;
+                }
+
+                //add something to db?
+            }
+        }
+    }
+}
