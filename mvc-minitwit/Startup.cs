b03011df78abb338cae9563765c28ff4cbe5ee8a
Frederik Volkers <frederikvolkers@hotmail.com>
2021-02-10 14:50:17 +0100

    ADD - some database files

--- mvc-minitwit/Startup.cs
+++ mvc-minitwit/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using mvc_minitwit.Data; //database context
+using Microsoft.EntityFrameworkCore;
 
 namespace mvc_minitwit
 {
@@ -24,6 +26,9 @@ namespace mvc_minitwit
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<MessageContext>(options =>
+            options.UseSqlite(Configuration.GetConnectionString("MessageContext"))); //database context for message
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
