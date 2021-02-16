5f5db93c56810ad521d628fb1c1c303a1667ce00
Alekxander Baxwill <abax@itu.dk>
2021-02-16 15:48:20 +0100

    added api controller file

---
+++ mvc-minitwit/Controllers/APIController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvc_minitwit.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class APIController : ControllerBase
+    {
+        
+
+        // https://localhost:5001/api
+        [HttpGet]
+        [Route("/api/")]
+        public IActionResult GetAllCities()
+        {
+            return Ok("hej");       //Ok returns in JSON-format
+        }
+
+        [HttpGet("/api/{startCh}")]
+        [Route("/api/latest")]
+        public IActionResult GetLatest()
+        {
+            return Ok("hej");       //Ok returns in JSON-format
+        }
+
+    }
+}
