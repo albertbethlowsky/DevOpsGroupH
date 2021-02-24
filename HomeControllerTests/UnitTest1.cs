273c8affc760eb033954ac04c8e8d9a5a6e8e44c
Alekxander Baxwill <abax@itu.dk>
2021-02-24 11:26:42 +0100

    added first draft unit test file

---
+++ HomeControllerTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using mvc_minitwit.Controllers;
+using System;
+using Xunit;
+
+
+namespace HomeControllerTests
+{
+    public class UnitTest1
+    {
+        private HomeController _homeController;
+
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
