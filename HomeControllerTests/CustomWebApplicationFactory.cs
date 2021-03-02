a796998be4dd1cffd60d0036fd162c177019486a
Alekxander Baxwill <abax@itu.dk>
2021-03-02 15:13:11 +0100

    Login-logout success test

--- HomeControllerTests/CustomWebApplicationFactory.cs
+++ HomeControllerTests/CustomWebApplicationFactory.cs
@@ -14,60 +14,6 @@ public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStar
 {
     private SqliteConnection Connection;
 
-    //protected override void ConfigureWebHost(IWebHostBuilder builder)
-    //{
-    //    //Connection = new SqliteConnection("DataSource=:memory:");
-    //    Connection = new SqliteConnection("DataSource=:memory:");
-
-    //    Connection.Open();
-
-    //    builder.UseEnvironment("Development");
-
-    //    builder.ConfigureTestServices(services =>
-    //    {
-    //        // Unregister existing database service (SQL Server).
-    //        var descriptor = services.SingleOrDefault(
-    //            d => d.ServiceType ==
-    //                typeof(DbContextOptions<MvcDbContext>));
-
-    //        if (descriptor != null) services.Remove(descriptor);
-
-    //        // Register new database service (SQLite In-Memory)
-    //        services.AddDbContext<MvcDbContext>(options => options.UseSqlite(Connection));
-
-
-    //        // Build the service provider.
-    //        var sp = services.BuildServiceProvider();
-
-
-    //        // Create a scope to obtain a reference to the database contexts
-    //        using (var scope = sp.CreateScope())
-    //        {
-    //            var scopedServices = scope.ServiceProvider;
-    //            var appDb = scopedServices.GetRequiredService<MvcDbContext>();
-
-    //            //var db = new DbContextOptionsBuilder<MvcDbContext>();
-
-    //            var logger = scopedServices.GetRequiredService<ILogger<CustomWebApplicationFactory<TStartup>>>();
-
-    //            // Ensure the database is created.
-    //            //appDb.Database.EnsureDeleted();
-    //            appDb.Database.EnsureCreated();
-
-    //            try
-    //            {
-    //                // Seed the database with some specific test data.
-    //                SeedData.PopulateTestData(appDb);
-    //            }
-    //            catch (Exception ex)
-    //            {
-    //                logger.LogError(ex, "An error occurred seeding the " +
-    //                                    "database with test messages. Error: {ex.Message}");
-    //            }
-    //        }
-    //    });
-
-    //}
 
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
--- HomeControllerTests/HomeControllerIntegrationTests.cs
+++ HomeControllerTests/HomeControllerIntegrationTests.cs
@@ -91,11 +91,15 @@ namespace HomeControllerTests
             }
         }
 
+        private void UserPrinter(User[] users)
+        {
+            foreach (User u in users)
+                output.WriteLine(u.username);
+        }
+
         [Fact]
         public async Task Register_Error_HaveToEnterPW()
         {
-            //var appF = new CustomWebApplicationFactory<MvcDbContext>();
-            //var _client = appF.CreateClient();
             _client = factory.CreateClient();
 
             dummyUser.pw_hash = null;
@@ -118,8 +122,7 @@ namespace HomeControllerTests
         [Fact]
         public async Task Register_Error_PWsDontMatch()
         {
-            //var appF = new CustomWebApplicationFactory<MvcDbContext>();
-            //var _client = appF.CreateClient();
+            
             _client = factory.CreateClient();
 
             dummyUser.pw_hash = "123";
@@ -133,15 +136,12 @@ namespace HomeControllerTests
         [Fact]
         public async Task Register_Error_InvalidEmail()
         {
-            //var appF = new CustomWebApplicationFactory<MvcDbContext>();
-            //var _client = appF.CreateClient();
             _client = factory.CreateClient();
 
             dummyUser.email = "noatsign";
             var resp = await _client.PostAsJsonAsync("/register", dummyUser);
             var strResp = await resp.Content.ReadAsStringAsync();
 
-            ResponsePrint(resp);
             Assert.Equal(HttpStatusCode.BadRequest, resp.StatusCode);
             Assert.Equal("You have to enter a valid email address", strResp);
 
@@ -157,7 +157,6 @@ namespace HomeControllerTests
         [Fact]
         public async Task Register_Error_UsernameAlreadyTaken()
         {
-            
             _client = factory.CreateClient();
 
             var initResp = await _client.PostAsJsonAsync("/register", dummyUser);
@@ -172,7 +171,6 @@ namespace HomeControllerTests
             Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
             //Assert.Equal("The username is already taken", strResponse);
             
-
         }
 
 
@@ -182,86 +180,71 @@ namespace HomeControllerTests
             
             _client = factory.CreateClient();
 
-            var users = _context.user.ToArray();
-            output.WriteLine("CONTEXT:");
-            foreach (User u in users)
-                output.WriteLine(u.username);
-
-
             dummyUser.username = "123dummy";
             var response = await _client.PostAsJsonAsync("/register", dummyUser);
            
             var stringResponse = await response.Content.ReadAsStringAsync();
             output.WriteLine(stringResponse);
 
-            users = _context.user.ToArray();
+            var users = _context.user.ToArray();
             output.WriteLine("CONTEXT:");
             foreach (User u in users)
                 output.WriteLine(u.username);
 
             //Assert.Equal("User registered", stringResponse);
-            //response.EnsureSuccessStatusCode();
-            
+            response.EnsureSuccessStatusCode();
+
         }
 
-        ////test_login_logout 
-        //[Fact]
-        //public async Task Login_LogOut()
-        //{
-        //    //var appF = new CustomWebApplicationFactory<MvcDbContext>();
-        //    //_client = appF.CreateClient();
-        //    _client = factory.CreateClient();
-        //    var resp = await _client.PostAsJsonAsync("/register", dummyUser);
-        //    resp.EnsureSuccessStatusCode();
-        //    var stringResponse = await resp.Content.ReadAsStringAsync();
-        //    output.WriteLine("REGISTER: " + stringResponse);
+        //test_login_logout 
+        [Fact]
+        public async Task Login_LogOut()
+        {
+            _client = factory.CreateClient();
+            dummyUser.username = "Login_LogOut_TestUser";
+            var resp = await _client.PostAsJsonAsync("/register", dummyUser);
+            resp.EnsureSuccessStatusCode();
+            var stringResponse = await resp.Content.ReadAsStringAsync();
+            output.WriteLine("REGISTER: " + stringResponse);
 
-        //    var scopeFactory = factory.Services.GetService<IServiceScopeFactory>();
+            UserPrinter(_context.user.ToArray());
 
-        //    using (var scope = scopeFactory.CreateScope())
-        //    {
-        //        var myDataContext = scope.ServiceProvider.GetService<MvcDbContext>();
-        //        //myDataContext.Database.EnsureCreated();
-        //        var lst = myDataContext.user;
-        //        output.WriteLine("InMem, All users - Before:");
-        //        foreach (User u in lst)
-        //            output.WriteLine(u.username);
-        //    }
+            var pw_hash = GetPW_hashFromUser(dummyUser.username);
+            output.WriteLine("pw_hash: " + pw_hash);
 
-        //    var pw_hash = GetPW_hashFromUser(dummyUser.username);
-        //    output.WriteLine("pw_hash: " + pw_hash);
+            var loginResp = await _client.GetAsync("/Home/SignIn?email=" + dummyUser.email + "&pw_hash=" + pw_hash);
+            loginResp.EnsureSuccessStatusCode();
 
-        //    //var userCredentials = new StringContent($"email ={ dummyUser.email } & pw_hash ={ pw_hash }");
+            var logOutResp = await _client.GetAsync("/Home/Sign_Out");
+            //var content = await logOutResp.Content.ReadAsStringAsync();
+            //ResponsePrint(logOutResp);
+            //output.WriteLine("LOGOUT content: " + content);        //gets back the html for public timeline
+            
+            logOutResp.EnsureSuccessStatusCode();
 
-        //    var loginResp = await _client.GetAsync("/Home/SignIn?email="+dummyUser.email+"&pw_hash="+pw_hash);
-        //    var content = await loginResp.Content.ReadAsStringAsync();
-        //    output.WriteLine("LOGIN content: "+ content);
+        }
 
-        //    loginResp.EnsureSuccessStatusCode();
+        [Fact]
+        public async Task CreateMessageByUser_Success()
+        {
+            var appF = new CustomWebApplicationFactory<MvcDbContext>();
+            _client = appF.CreateClient();
 
-        //}
+            await _client.PostAsJsonAsync("/register", dummyUser);
 
-        //[Fact]
-        //public async Task CreateMessageByUser_Success()
-        //{
-        //    var appF = new CustomWebApplicationFactory<MvcDbContext>();
-        //    _client = appF.CreateClient();
+            var request = new HttpRequestMessage(HttpMethod.Post, "msgs/" + dummyUser.username);
 
-        //    await _client.PostAsJsonAsync("/register", dummyUser);
+            //request.Content = new StringContent(JsonSerializer.Serialize(new {
+            //    term = "MFA",
+            //    definition = "An authentication process that considers multiple factors."
+            //}), Encoding.UTF8, "application/json");
 
-        //    var request = new HttpRequestMessage(HttpMethod.Post, "msgs/" + dummyUser.username);
+            // Act
+            var response = await _client.SendAsync(request);
 
-        //    //request.Content = new StringContent(JsonSerializer.Serialize(new {
-        //    //    term = "MFA",
-        //    //    definition = "An authentication process that considers multiple factors."
-        //    //}), Encoding.UTF8, "application/json");
-
-        //    // Act
-        //    var response = await _client.SendAsync(request);
-
-        //    var mess = new Message { author_id = 0, text = "test text", pub_date = (int)(DateTimeOffset.Now.ToUnixTimeSeconds()) };
-        //    //_client.PostAsync("/msgs/"+dummyUser.username, mess);
-        //}
+            var mess = new Message { author_id = 0, text = "test text", pub_date = (int)(DateTimeOffset.Now.ToUnixTimeSeconds()) };
+            //_client.PostAsync("/msgs/"+dummyUser.username, mess);
+        }
 
 
         //[Fact]
