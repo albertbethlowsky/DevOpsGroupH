66d8a9d7b57c2877c7286320a346049810bcfd0c
alex-bax <alex.tennis@live.dk>
2021-03-29 17:47:35 +0200

    gh release 2

--- mvc-minitwit/Startup.cs
+++ mvc-minitwit/Startup.cs
@@ -30,7 +30,7 @@ namespace mvc_minitwit
         private readonly IWebHostEnvironment Environment;
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        //test gh release tag
+        //test gh release tag 3
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
