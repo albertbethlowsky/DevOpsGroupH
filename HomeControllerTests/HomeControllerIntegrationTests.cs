c31c6ae816fe561cc2c91daebe825e3842276994
frvo <frvo@itu.dk>
2021-04-19 09:51:51 +0200

    commented out homecontrollertest
    Signed-off-by: frvo <frvo@itu.dk>

--- HomeControllerTests/HomeControllerIntegrationTests.cs
+++ HomeControllerTests/HomeControllerIntegrationTests.cs
@@ -1,4 +1,4 @@
-using mvc_minitwit;
+﻿using mvc_minitwit;
 using mvc_minitwit.Data;
 using mvc_minitwit.Models;
 using Newtonsoft.Json;
@@ -57,14 +57,14 @@ namespace HomeControllerTests
         public async Task test_TimeLine()
         {
             //_client = factory.CreateClient();
-            var logger = _scope.ServiceProvider.GetRequiredService<ILogger<HomeController>>();
-            HomeController hc = new HomeController(logger, _context);
+            // var logger = _scope.ServiceProvider.GetRequiredService<ILogger<HomeController>>();
+            // HomeController hc = new HomeController(logger, _context);
             
 
-            // 2 - Act
-			var actionResult = await hc.Timeline(dummyUser.username) as ViewResult; // Call the edit view with no item Id (Add New).
-            // Assert
-            Console.WriteLine(actionResult.ViewBag.message);
+            // // 2 - Act
+			// var actionResult = await hc.Timeline(dummyUser.username) as ViewResult; // Call the edit view with no item Id (Add New).
+            // // Assert
+            // Console.WriteLine(actionResult.ViewBag.message);
             //Assert.Equal(actionResult.ViewBag.message, "My message.");
 
            // Assert.Equal(actionResult.ViewName, "MyView");
---
+++ SLA.md
@@ -0,0 +1,4 @@
+# SLA for Neutrals Minitwit
+
+Azure DevOps SLA: https://azure.microsoft.com/en-us/support/legal/sla/azure-devops/v2_0/
+
